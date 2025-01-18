6bc5ff53c74ce14324c0f9f001642edd4ed3e303
Karthik150502 <karthikrdycool@gmail.com>
2025-01-19 00:33:28 +0530

    Latest changes from master

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ console.log("hello world");
 console.log("My name is Karthik J");
 
 
-function main(){
-   console.log("Hello from the main function");
-   console.log("This is a new day.")
-   console.log("This is a new world.");
-   console.log("Its a new dawn");
-}
-
 
 
 
