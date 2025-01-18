2bda7de95769cc567b551d4e68a88029c88b62fa
Karthik150502 <karthikrdycool@gmail.com>
2025-01-18 22:59:42 +0530

    A new commit from latest2

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ console.log("My name is Karthik J");
 function main(){
    console.log("Hello from the main function");
    console.log("This is a new day.")
+   console.log("This is a new world.");
 }
 
 
