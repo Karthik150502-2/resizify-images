71409a82aa6b6cde41676a05501942a9c4de1502
Karthik150502 <karthikrdycool@gmail.com>
2025-01-19 00:54:40 +0530

    Latest from master

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("hello world");
 console.log("My name is Karthik J");
-
-console.log("The new change from thge master branch");
+console.log("The new change from the master branch 1");
+console.log("The new change from the master branch 2");
 
 
 
