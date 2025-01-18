ff40d8d9bef615ba1821563ee2c8989f6dd29108
Karthik150502 <karthikrdycool@gmail.com>
2025-01-19 00:03:16 +0530

    A new second commit from latest2

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function main(){
    console.log("Hello from the main function");
    console.log("This is a new day.")
    console.log("This is a new world.");
+   console.log("Its a new dawn");
 }
 
 
