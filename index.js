a424e821439419640ee07b551bc00366677bbb24
Karthik150502 <karthikrdycool@gmail.com>
2025-01-19 13:28:44 +0530

    Added a factorial function

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ console.log("The new change from the master branch 2");
 console.log("The changes from the feat branch......");
 
 
-console.log(1+5)
-console.log(7+8)
+console.log(1 + 5)
+console.log(7 + 8)
 
+function factorial(n) {
+    let res = 1;
+    if (n === 1 || n === 0) {
+        return 1
+    };
+    for(let i=1;i<=n;i++){
+        res = res * i 
+    }
+    return res;
+}
+console.log(factorial(6))
+console.log(factorial(5))
+console.log(factorial(4))
