64869f472f16369b791eae87e212fc512d2ce570
Karthik150502 <karthikrdycool@gmail.com>
2025-01-19 16:33:20 +0530

    Added multiply function

--- index.js
+++ index.js
@@ -7,4 +7,36 @@ console.log("The changes from the feat branch......");
 
 console.log(1+5)
 console.log(7+8)
+console.log(1 + 5)
+console.log(7 + 8)
 
+
+
+function fibonacci() {
+    let prev = 0;
+    let curr = 1;
+    return () => {
+        let temp = prev;
+        prev = curr; 
+        curr = temp + curr; 
+        return temp;
+    }
+}
+let fib = fibonacci()
+
+console.log(fib())
+console.log(fib())
+console.log(fib())
+console.log(fib())
+console.log(fib())
+
+function multiplyBy(n){
+    return (m)=>{
+        return m * n
+    }
+}
+
+
+let mulBy2 = multiplyBy(2)
+console.log(mulBy2(5))
+console.log(mulBy2(15))
